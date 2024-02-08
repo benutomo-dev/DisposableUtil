94829be04b3265dd41e1ac6abd300892a41097df
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2024-02-08 13:26:32 +0900

    プロジェクト ファイルを追加します。

---
+++ DisposableUtil/CompositeDisposable.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Benutomo.DisposableUtil;
+
+public class CompositeDisposable : ICollection<IDisposable>, IDisposable
+{
+    public int Count
+    {
+        get
+        {
+            var count = Volatile.Read(ref _count);
+            if (count < 0)
+            {
+                throw new ObjectDisposedException(null);
+            }
+            return count;
+        }
+    }
+
+    public bool IsReadOnly => false;
+
+
+    private List<IDisposable>? _disposables;
+
+    private ReadOnlyCollection<IDisposable>? _enumerableCache;
+
+    private int _count;
+
+    public CompositeDisposable(params IDisposable[] disposables)
+    {
+        _disposables = disposables.ToList();
+        _count = _disposables.Count;
+    }
+
+    public void Dispose()
+    {
+        var disposables = Interlocked.Exchange(ref _disposables, null);
+
+        if (disposables is null) return;
+
+        List<Exception>? exceptions = null;
+
+        foreach (var disposable in disposables)
+        {
+            try
+            {
+                disposable?.Dispose();
+            }
+            catch (Exception ex)
+            {
+                exceptions ??= new List<Exception>();
+                exceptions.Add(ex);
+            }
+        }
+
+        if (exceptions is not null)
+        {
+            throw new AggregateException("Exception was thrown in the call to Dispose.", exceptions);
+        }
+    }
+
+    public void Add(IDisposable item)
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        lock(gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            _enumerableCache = null;
+            _disposables.Add(item);
+        }
+    }
+
+    public bool Remove(IDisposable item)
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        lock (gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            _enumerableCache = null;
+            return _disposables.Remove(item);
+        }
+    }
+
+    public void Clear()
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        lock (gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            _enumerableCache = null;
+            _disposables.Clear();
+        }
+    }
+
+    public bool Contains(IDisposable item)
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        lock (gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            return _disposables.Contains(item);
+        }
+    }
+
+    public void CopyTo(IDisposable[] array, int arrayIndex)
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        lock (gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            _disposables.CopyTo(array, arrayIndex);
+        }
+    }
+
+    public IEnumerator<IDisposable> GetEnumerator()
+    {
+        var gate = Volatile.Read(ref _disposables);
+
+        if (gate is null)
+        {
+            throw new ObjectDisposedException(null);
+        }
+
+        ReadOnlyCollection<IDisposable> enumerable;
+        lock (gate)
+        {
+            if (_disposables is null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            if (_enumerableCache is null)
+            {
+                _enumerableCache = new ReadOnlyCollection<IDisposable>(_disposables.ToArray());
+            }
+
+            enumerable = _enumerableCache;
+        }
+
+        return enumerable.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ DisposableUtil/Disposable.cs
@@ -0,0 +1,72 @@
+﻿namespace Benutomo.DisposableUtil;
+
+public static class Disposable
+{
+    /// <summary>
+    /// 何もしないダミー用<see cref="IDisposable"/>。
+    /// </summary>
+    public static IDisposable Empty { get; } = new EmptyDisposable();
+
+    /// <summary>
+    /// <see cref="IDisposable.Dispose"/>で<paramref name="disposeAction"/>を呼び出す<see cref="IDisposable"/>を作成する。
+    /// </summary>
+    /// <param name="disposeAction"><see cref="IDisposable.Dispose"/>で呼びされる処理</param>
+    /// <returns><see cref="IDisposable"/></returns>
+    public static IDisposable Create(Action disposeAction)
+    {
+        return new ActionDisposable(disposeAction);
+    }
+
+    /// <summary>
+    /// usingブロックまたはusing宣言用にその場でクリーンアップ処理を呼び出すref structのDisposableを作成する。
+    /// </summary>
+    /// <typeparam name="T">クリーンアップ処理のパラメータの型</typeparam>
+    /// <param name="disposeAction">クリーンアップ処理</param>
+    /// <param name="actionArg">クリーンアップ処理のパラメータ</param>
+    /// <returns>クリーンアップ処理を呼び出すref structのDisposable</returns>
+    public static RefStructDisposable<T> UsingWithCleanup<T>(Action<T> disposeAction, T actionArg)
+    {
+        return new RefStructDisposable<T>(disposeAction, actionArg);
+    }
+
+    private class EmptyDisposable : IDisposable
+    {
+        public void Dispose() { }
+    }
+
+    private class ActionDisposable : IDisposable
+    {
+        public Action? _disposeAction;
+
+        public ActionDisposable(Action? disposeAction)
+        {
+            _disposeAction = disposeAction;
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref _disposeAction, null)?.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// クリーンアップ処理を呼び出すためのref struct
+    /// </summary>
+    /// <typeparam name="T">クリーンアップ処理のパラメータの型</typeparam>
+    public ref struct RefStructDisposable<T>
+    {
+        public Action<T>? _disposeAction;
+        public T _actionArg;
+
+        internal RefStructDisposable(Action<T> disposeAction, T actionArg)
+        {
+            _disposeAction = disposeAction ?? throw new ArgumentNullException(nameof(disposeAction));
+            _actionArg = actionArg;
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref _disposeAction, null)?.Invoke(_actionArg);
+        }
+    }
+}
