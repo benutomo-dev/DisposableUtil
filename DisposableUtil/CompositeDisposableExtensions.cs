111a0f0209d54902a97a70b3c266a0c5b0c21eaf
benutomo <52617232+benutomo-dev@users.noreply.github.com>
2024-02-08 14:16:54 +0900

    WithAddTo拡張メソッドを追加

---
+++ DisposableUtil/CompositeDisposableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Benutomo.DisposableUtil;
+
+public static class CompositeDisposableExtensions
+{
+    public static T WithAddTo<T>(this T disposable, CompositeDisposable compositeDisposable) where T : IDisposable
+    {
+        compositeDisposable.Add(disposable);
+        return disposable;
+    }
+}
